851745eedd407dfcfb6552dd64b90ceb556904f4
Miguel Pazatto <miguelpazatto60@gmail.com>
2026-04-10 16:14:35 -0300

    Service layer, component registration

--- src/main/java/com/miguelpazatto/course/repositories/UserRepository.java
+++ src/main/java/com/miguelpazatto/course/repositories/UserRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.miguelpazatto.course.entities.User;
 
+//nesse caso notation de component não é necessária pela relação de herança
 public interface UserRepository extends JpaRepository<User, Long> {
 	
 }
